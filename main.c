9eabfd8b63c876e3a9278f7d4c904f0676354efe
Asha Ryan <asha.ryan@uon.edu.com>
2019-04-04 05:25:04 +0000

    Working up until letters towards the end of the alphabet.

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
+char RotoEncryptLetter(char input, int key);
+
 int main() {
-    printf("Hello World");
-    return 0;
+    char message[] = "abcdefghijklmnopqrstuvwxyz";
+    char encrypted[100];
+
+    
+    for (int i=0 ; i<26 ; i++) {
+      encrypted[i] = RotoEncryptLetter(message[i], 3);
+    }
+
+    printf("Encrypted Alphabet: %c %c %c %c\n", encrypted[0], encrypted[1], encrypted[2], encrypted[3]);
+    printf("Encrypted String: %s", encrypted);
+    
+}
+
+/* this function should ensure the input is uppercase only*/
+char RotoEncryptLetter(char input, int key) {
+   
+   if ((input > 64) && (input < 91)){
+       input = input + key;
+   } else if ((input > 96) && (input < 123)) {
+       input = input - 32;
+       input = input + key;
+   } 
+          
+return input;
 }
