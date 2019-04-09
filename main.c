3698ec1671d6bf5f739b130592ee7558728fb84d
Asha Ryan <asha.ryan@uon.edu.com>
2019-04-09 02:46:28 +0000

    trying to get file I/O working

--- main.c
+++ main.c
@@ -4,16 +4,27 @@
 char RotoEncryptLetter(char input, int key);
 char RotoDecryptLetter(char input, int key);
 
+
 int main() {
     char message[100];
     char encrypted[100];
     
-    /* scan in input.txt and assign to message*/
+    FILE *input;
+    input = fopen("input.txt", "r");
+    while(feof (input) == 0) {
+        char t;
+        //read a character
+        fscanf(input, "%c", &t);
+        // print that character to the console 
+        printf("%c\n", t);
+    }
+    
+    /* scan in input.txt and assign to message for rotation cipher*/
     for (int i=0; i<100; i++) {
         scanf("%[^\n]s", message); 
         encrypted[i] = RotoEncryptLetter(message[i], 3);
     }
-    printf("Encrypted String: %s\n", encrypted);
+    printf("Encrypted Rotation String: %s\n", encrypted);
     
     char decrypted[100];
     
@@ -21,7 +32,7 @@ int main() {
     for (int c=0; c<100; c++) {
         decrypted[c] = RotoDecryptLetter(encrypted[c], 3);
     }
-    printf("Decrypted String: %s\n", decrypted);
+    printf("Decrypted Rotation String: %s\n", decrypted);
     
 }
 
@@ -57,3 +68,4 @@ char RotoDecryptLetter(char input, int key) {
     
 return input;
 }
+
